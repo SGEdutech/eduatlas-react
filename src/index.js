9caf9dd4f6c44baa01b84610b222ed2ea5c1aeb0
16bitash <entropy.ash@gmail.com>
2019-01-21 21:40:19 +0530

    Modified index.js to work with cordova

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const startApp = () => ReactDOM.render(<App />, document.getElementById('root'));
+
+if (window.cordova) {
+	document.addEventListener('deviceready', startApp, false);
+	serviceWorker.register();
+} else {
+	startApp();
+	serviceWorker.unregister();
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
