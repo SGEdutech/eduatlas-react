7b6e2e28dddce3e5ebdefc643a6eae12e09f2dbd
16bitash <entropy.ash@gmail.com>
2019-01-21 11:37:01 +0530

    Fix: Error tab-bar-height.json not found

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React, { Component } from 'react';
 import { StickyContainer, Sticky } from 'react-sticky';
-import { tabBarHeight } from './tab-bar-height.json';
 
 import Nav1Content from './components/Nav1Content';
 
