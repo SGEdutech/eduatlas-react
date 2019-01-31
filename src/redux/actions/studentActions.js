00254c3aa5fe386b4055b051b4790603c7ce2a27
16bitash <entropy.ash@gmail.com>
2019-01-31 17:58:57 +0530

    Add: Installments actions

--- src/redux/actions/studentActions.js
+++ src/redux/actions/studentActions.js
@@ -53,3 +53,32 @@ export function deletePayment(studentId, paymentId) {
 		});
 	};
 }
+
+export function addInstallment(newInstallment, studentId, paymentId) {
+	return dispatch => {
+		dispatch({
+			type: 'ADD_INSTALLMENT',
+			payload: axios.post(`https://eduatlas.com/tuition/5bbe191a64512a2f77b84c70/student/${studentId}/payment/${paymentId}/installment`, newInstallment)
+		});
+	};
+}
+
+export function editInstallment(editedInstallment, studentId, paymentId, installmentId) {
+	return dispatch => {
+		dispatch({
+			type: 'EDIT_INSTALLMENT',
+			payload: axios.put(`https://eduatlas.com/tuition/5bbe191a64512a2f77b84c70/student/${studentId}/payment/${paymentId}/installment/${installmentId}`, editedInstallment)
+		});
+	};
+}
+
+
+
+export function deleteInstallment(studentId, paymentId) {
+	return dispatch => {
+		dispatch({
+			type: 'DELETE_INSTALLMENT',
+			payload: axios.delete(`https://eduatlas.com/tuition/5bbe191a64512a2f77b84c70/student/${studentId}/payment/${paymentId}/installment/${installmentId}`)
+		});
+	};
+}
