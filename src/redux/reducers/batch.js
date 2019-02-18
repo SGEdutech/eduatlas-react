363db6f8137422cbb5bdf21e9f4284e3655ac272
16bitash <entropy.ash@gmail.com>
2019-02-18 21:10:38 +0530

    Fix: Students not being added when directly added from request

--- src/redux/reducers/batch.js
+++ src/redux/reducers/batch.js
@@ -4,26 +4,38 @@ const initState = {
 
 function batchReducer(state = initState, action) {
 	switch (action.type) {
-		case 'FETCH_ALL_FULFILLED':
+		case 'FETCH_ALL_FULFILLED': {
 			return { ...state, batches: action.payload.data.batches };
-		case 'ADD_BATCH_FULFILLED':
+		}
+		case 'ADD_BATCH_FULFILLED': {
 			return { ...state.batches, batches: [...state.batches, action.payload.data] };
+		}
 		case 'EDIT_BATCH_FULFILLED': {
 			const editedBatch = action.payload.data;
 			const { _id } = editedBatch;
 			const newBatches = state.batches.map(batch => batch._id === _id ? editedBatch : batch);
 			return { ...state, batches: newBatches };
 		}
-		case 'DELETE_BATCH_FULFILLED':
+		case 'DELETE_BATCH_FULFILLED': {
 			return { ...state, batches: state.batches.filter(batch => batch._id !== action.payload.data._id) };
-		case 'DELETE_COURSE_FULFILLED':
+		}
+		case 'DELETE_COURSE_FULFILLED': {
 			return { ...state, batches: state.batches.filter(batch => batch.courseId !== action.payload.data._id) };
-		// TODO: Delete batch students on institute students delete
-		case 'DELETE_STUDENT_FULFILLED':
+		}
+		case 'ADD_STUDENT_FULFILLED': {
+			const { _id: addedStudentId, batchAdded } = action.payload.data;
+			const batches = [...state.batches];
+			batches.forEach(batch => {
+				if (batch._id === batchAdded) batch.students.push(addedStudentId);
+			});
+			return { ...state, batches };
+		}
+		case 'DELETE_STUDENT_FULFILLED': {
 			const { _id: deletedStudentId } = action.payload.data;
 			const batches = [...state.batches];
 			batches.forEach(batch => batch.students.filter(studentId => studentId === deletedStudentId));
 			return { ...state, batches };
+		}
 		default:
 			return state;
 	}
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,12 +1,11 @@
 const initState = {
-	userInfo: { primaryEmail: 'test@gmail.com' }
+	userInfo: {}
 };
 
 function userReducer(state = initState, action) {
 	switch (action.type) {
 		case 'FETCH_ALL_FULFILLED':
 			return state;
-			return { ...state, userInfo: action.payload.data.user };
 		case 'USER_LOGIN_FULFILLED':
 			return { ...state, userInfo: action.payload.data };
 		case 'USER_LOGOUT_FULFILLED':
