150277a6bc8937e71600aa66e6440efc84377fd1
16bitash <entropy.ash@gmail.com>
2019-02-22 12:23:49 +0530

    Make fetchall send credentials

--- src/redux/actions/fetchAllAction.js
+++ src/redux/actions/fetchAllAction.js
@@ -3,7 +3,7 @@ import { tuitionId } from '../../config.json';
 
 export default () => dispatch => {
 	dispatch({
-		payload: axios.get(`https://eduatlas.com/tuition/${tuitionId}/dashboard`),
+		payload: axios.get(`https://eduatlas.com/tuition/${tuitionId}/dashboard`, { withCredentials: true }),
 		type: 'FETCH_ALL'
 	});
 };
