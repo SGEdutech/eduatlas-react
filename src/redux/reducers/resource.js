01654377acbfc05b6daddb3d7f2e57ce11a5111e
16bitash <entropy.ash@gmail.com>
2019-03-01 14:41:41 +0530

    Fix: Resources not being added dynamicly

--- src/redux/reducers/resource.js
+++ src/redux/reducers/resource.js
@@ -4,10 +4,17 @@ const initState = {
 
 function resourceReducer(state = initState, action) {
 	switch (action.type) {
-		case 'FETCH_ALL_FULFILLED':
+		case 'FETCH_ALL_FULFILLED': {
 			return { ...state, resources: action.payload.data.resources };
-		case 'DELETE_RESOURCE_FULFILLED':
+		}
+		case 'ADD_RESOURCE_FULFILLED': {
+			const resources = [...state.resources];
+			resources.push(action.payload.data);
+			return { ...state, resources };
+		}
+		case 'DELETE_RESOURCE_FULFILLED': {
 			return { ...state, resources: state.resources.filter(resource => resource._id !== action.payload.data._id) };
+		}
 		default:
 			return state;
 	}
