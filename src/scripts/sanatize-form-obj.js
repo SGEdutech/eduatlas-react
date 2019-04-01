cd2c7e080a1b4bbceb4fc40e114b13e30e51c5c5
16bitash <entropy.ash@gmail.com>
2019-04-01 13:01:34 +0530

    Allow input 0 to be sent to the server

--- src/scripts/sanatize-form-obj.js
+++ src/scripts/sanatize-form-obj.js
@@ -2,6 +2,6 @@ export default obj => {
 	const keys = Object.keys(obj);
 	keys.forEach(key => {
 		const value = obj[key];
-		if (Boolean(value) === false) delete obj[key];
+		if (value !== 0 && Boolean(value) === false) delete obj[key];
 	});
 };
