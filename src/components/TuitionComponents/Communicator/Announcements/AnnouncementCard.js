dd4d3794eeba839aa43dff42a87e0e987434fc8f
mannvender <mannvender001@gmail.com>
2019-04-12 13:34:21 +0530

    Add View Announcement component, reverse notifications array before rendering

--- src/components/TuitionComponents/Communicator/Announcements/AnnouncementCard.js
+++ src/components/TuitionComponents/Communicator/Announcements/AnnouncementCard.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-relative-link';
 
 import moment from 'moment';
 
-import { Row, Col, Card } from 'antd';
+import IconsWithTooltip from '../../../SharedComponents/IconsWithTooltip';
+
+import {
+	Card,
+	Col,
+	Row
+} from 'antd';
 
 function AnnouncementCard(props) {
-	const { message, receivers, createdAt } = props;
+	const { createdAt, id, message, receivers } = props;
+
+	const iconsArray = [
+		<Link to={'./view-announcement/' + id}><IconsWithTooltip tooltipMessage="View Announcement" iconType="eye" /></Link>
+	];
 
 	return (
 		<Card className="mb-3"
+			actions={iconsArray}
 			title={<div className="text-capitalize"><span className="font-weight-bold">{moment(createdAt).fromNow()}</span></div>}>
 			<Row>
 				<Col>
-					<div><span className="font-weight-bold">Message: </span> {message}</div>
+					<div className="one-line-ellipsis"><span className="font-weight-bold">Message: </span> {message}</div>
 				</Col>
 				<Col>
 					<div><span className="font-weight-bold">No. Of Receivers: </span> {receivers.length}</div>
