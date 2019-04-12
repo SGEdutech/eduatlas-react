7bf57d246c33e6a2cd26029e4503f435555a557b
16bitash <entropy.ash@gmail.com>
2019-04-12 19:40:19 +0530

    Fix: Cloned object function changing moment to date of orginal object

--- src/scripts/cloneObj.js
+++ src/scripts/cloneObj.js
@@ -1,8 +1,6 @@
 import dateToMoment from './dateToMoment';
-import momentToDate from './momentToDate';
 
 export default obj => {
-	momentToDate(obj);
 	obj = JSON.parse(JSON.stringify(obj));
 	dateToMoment(obj);
 	return obj;
