18ba0114cf3ce3aecf4df2198e40255e90a0f273
16bitash <entropy.ash@gmail.com>
2019-04-12 16:57:30 +0530

    Add: Prod and dev config

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,20 @@
 import { applyMiddleware, createStore, compose } from 'redux';
+import { isProd } from '../config.json';
 
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
 
 import reducer from './reducers/rootReducer';
 
-const middleware = window.cordova ?
-	compose(applyMiddleware(promise(), thunk)) :
-	compose(applyMiddleware(promise(), thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const prodMiddleware = compose(applyMiddleware(promise(), thunk));
+const devMiddleware = compose(applyMiddleware(promise(), thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-export default createStore(reducer, middleware);
+let store;
+
+if (isProd) {
+	store = createStore(reducer, prodMiddleware);
+} else {
+	store = createStore(reducer, devMiddleware);
+}
+
+export default store;
