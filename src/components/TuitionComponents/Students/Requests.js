9aeef78564fb7e7fd7eb0ead5a0bf0bba97ad94a
16bitash <entropy.ash@gmail.com>
2019-04-23 18:28:29 +0530

    Add: Search in requests

--- src/components/TuitionComponents/Students/Requests.js
+++ src/components/TuitionComponents/Students/Requests.js
@@ -5,6 +5,8 @@ import RequestCard from './Requests/RequestCard';
 import {
 	Col,
 	Empty,
+	Icon,
+	Input,
 	Row
 } from 'antd';
 
@@ -17,6 +19,12 @@ const colLayout = {
 };
 
 class Requests extends Component {
+	state = {
+		search: ''
+	}
+
+	handleSearchInpChange = e => this.setState({ search: e.currentTarget.value });
+
 	render() {
 		const { requests, addStudent, deleteRequest, batches, courses, students } = this.props;
 
@@ -24,14 +32,23 @@ class Requests extends Component {
 			image="https://gw.alipayobjects.com/mdn/miniapp_social/afts/img/A*pevERLJC9v0AAAAAAAAAAABjAQAAAQ/original"
 			description={<span>Nothing is better than something...</span>}></Empty>;
 
+		const requestToRender = requests.filter(({ name, email }) => {
+			const { search } = this.state;
+			const searchRegex = new RegExp(search, 'i');
+			return searchRegex.test(name) || searchRegex.test(email);
+		});
+
 		const requestCardsJsx = (
-			requests.map(request => <Col {...colLayout} key={request._id}><RequestCard requestInfo={request} deleteRequest={deleteRequest} addStudent={addStudent} batches={batches} courses={courses} students={students} /></Col>)
+			requestToRender.map(request => <Col {...colLayout} key={request._id}><RequestCard requestInfo={request} deleteRequest={deleteRequest} addStudent={addStudent} batches={batches} courses={courses} students={students} /></Col>)
 		);
 		return (
 			<>
 				<div className="container">
+					<Row className="mb-3">
+						<Input allowClear addonAfter={<Icon type="search" />} onChange={this.handleSearchInpChange} placeholder="Search Students" />
+					</Row>
 					<Row gutter={16}>
-						{requests.length === 0 ? emptyJsx : requestCardsJsx}
+						{requestToRender.length === 0 ? emptyJsx : requestCardsJsx}
 					</Row>
 				</div>
 			</>
